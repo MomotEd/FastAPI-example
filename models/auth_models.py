1828f2e191c8fb8fd0fb2bbb113ceb67359a5cf1
vkurta <valentyn.kurta@dataart.com>
2023-12-24 11:46:11 +0100

    commit 1.0: fastapi+flake+precommit +SQLmodel+alembic with the first generation and migration into DB

---
+++ models/auth_models.py
@@ -0,0 +1,30 @@
+import datetime
+from typing import Optional
+
+from pydantic import validator, EmailStr
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True)
+    email: str
+    username: str = Field(index=True)
+    first_name: str
+    last_name: str
+    registered_at: datetime.datetime = datetime.datetime.now()
+    password: str = Field(max_length=256, min_length=6)
+    is_active: bool = False
+    is_superuser: bool = False
+    is_verified: bool = False
+
+
+class UserInput(SQLModel):
+    username: str
+    password: str = Field(max_length=256, min_length=6)
+    email: str
+    is_seller: bool = False
+
+
+class UserLogin(SQLModel):
+    username: str
+    password: str
